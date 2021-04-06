c9ea5bd8498f0b4570756d66536d7bccef8fef94
Daniel Morales <danisan98@hotmail.es>
2021-04-05 21:18:47 -0500

    Prueba de commit inicial

---
+++ app/src/main/java/com/example/rankinguprimeraentrga/view/SignUp.java
@@ -0,0 +1,18 @@
+package com.example.rankinguprimeraentrga.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rankinguprimeraentrga.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+}
